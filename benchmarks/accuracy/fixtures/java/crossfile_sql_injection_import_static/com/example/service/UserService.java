3153595b999df143cdb14cdfdb6e6a15d86cdde2
Sandeepan Nandi <priyankasentunandi@gmail.com>
2026-09-24 12:50:45 -0500

    Create UserService.java

---
+++ benchmarks/accuracy/fixtures/java/crossfile_sql_injection_import_static/com/example/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import java.sql.*;
+
+public class UserService {
+    private static Connection conn;
+
+    public static ResultSet findByName(String name) throws SQLException {
+        String sql = "SELECT * FROM users WHERE name = '" + name + "'";
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery(sql);
+    }
+}
