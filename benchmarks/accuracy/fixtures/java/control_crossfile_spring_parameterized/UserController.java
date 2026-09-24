f5c0943304db6da48c144a16e0c57768f0517b93
Sandeepan Nandi <priyankasentunandi@gmail.com>
2026-09-24 16:01:04 -0500

    Create UserController.java

---
+++ benchmarks/accuracy/fixtures/java/control_crossfile_spring_parameterized/UserController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.sql.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @GetMapping("/user/{name}")
+    public ResultSet getUser(@PathVariable String name) throws SQLException {
+        return UserService.findByName(name);
+    }
+}
