db225ad710f23f639c8116637379218c9c60ee2c
Sandeepan Nandi <priyankasentunandi@gmail.com>
2026-09-24 08:59:09 -0500

    Create main.go

---
+++ benchmarks/accuracy/fixtures/go/control_crossfile_module_parameterized_sql/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/shop/store"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	store.FindUser(name)
+}
